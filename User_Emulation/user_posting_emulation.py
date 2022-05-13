eef7a360ca4c37bb5c142b7081b257c31d80c340
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-14 01:15:52 +0530

    fixed tabs and spaces issue

--- User_Emulation/user_posting_emulation.py
+++ User_Emulation/user_posting_emulation.py
@@ -6,22 +6,21 @@ import boto3
 import json
 import sqlalchemy
 
-
 random.seed(100)
 
 
 class AWSDBConnector:
 
     def __init__(self):
-
         self.HOST = "pinterestdbreadonly.cq2e8zno855e.eu-west-1.rds.amazonaws.com"
         self.USER = 'project_user'
         self.PASSWORD = ':t%;yCY3Yjg'
         self.DATABASE = 'pinterest_data'
         self.PORT = 3306
-        
+
     def create_db_connector(self):
-        engine = sqlalchemy.create_engine(f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
+        engine = sqlalchemy.create_engine(
+            f"mysql+pymysql://{self.USER}:{self.PASSWORD}@{self.HOST}:{self.PORT}/{self.DATABASE}?charset=utf8mb4")
         return engine
 
 
@@ -35,7 +34,7 @@ def run_infinite_post_data_loop():
         engine = new_connector.create_db_connector()
         selected_row = engine.execute(f"SELECT * FROM pinterest_data LIMIT {random_row}, 1")
         for row in selected_row:
-	    result = dict(row)
+            result = dict(row)
         requests.post("http://localhost:8000/pin/", json=result)
         print(result)
 
@@ -43,7 +42,3 @@ def run_infinite_post_data_loop():
 if __name__ == "__main__":
     run_infinite_post_data_loop()
     print('Working')
-    
-    
-
-
