90114333212f506d0029a1664d406c39743b4417
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-14 02:16:09 +0530

    Stored all required commands in bash folder

---
+++ bash/api-commands.sh
@@ -0,0 +1,5 @@
+# Commands for Pinterest App
+python API/project_pin_API.py
+python User_Emulation/user_posting_emulation.py
+python Consumers/batch_consumer.py
+python Consumers/streaming_consumer.py
