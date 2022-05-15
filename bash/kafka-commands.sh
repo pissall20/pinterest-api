8261ca5f75aa913e350f22553973bc3553e89352
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-15 18:29:52 +0530

    Changed kafka topic

--- bash/kafka-commands.sh
+++ bash/kafka-commands.sh
@@ -4,9 +4,9 @@
 zookeeper-server-start /usr/local/etc/kafka/zookeeper.properties
 # Start kafka server
 kafka-server-start /usr/local/etc/kafka/server.properties
-# Create kafka topic named test-topic
-kafka-topics --create --topic test-topic --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
+# Create kafka topic named pinterest
+kafka-topics --create --topic pinterest --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1
 # Create kafka producer - You can write messages into this (ONLY FOR TESTING)
-kafka-console-producer --broker-list localhost:9092 --topic test-topic
+kafka-console-producer --broker-list localhost:9092 --topic pinterest
 # Create kafka consumer - You can view messages in this terminal (ONLY FOR TESTING)
-kafka-console-consumer --bootstrap-server localhost:9092 --topic test-topic --from-beginning
+kafka-console-consumer --bootstrap-server localhost:9092 --topic pinterest --from-beginning
