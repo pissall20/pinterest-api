3619afb33b2df53067c946d0dbd1072fc0dfef7d
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-16 20:21:36 +0530

    Batch consumer completion

--- Consumers/batch_consumer.py
+++ Consumers/batch_consumer.py
@@ -1,3 +1,4 @@
+# To fix path issues
 import sys, os
 sys.path.insert(0, os.getcwd())
 
@@ -6,7 +7,7 @@ from json import loads, dumps
 import boto3
 from settings import *
 
-
+# Create a Kafka Consumer
 consumer = KafkaConsumer(
     bootstrap_servers=['localhost:9092'],
     auto_offset_reset='earliest',
@@ -14,6 +15,7 @@ consumer = KafkaConsumer(
     value_deserializer=lambda message: loads(message.decode('utf-8'))
 )
 
+# Create s3 object to interact with s3 bucket
 s3 = boto3.resource(
     service_name='s3',
     region_name=region,
@@ -21,12 +23,17 @@ s3 = boto3.resource(
     aws_secret_access_key=aws_secret_key
 )
 
+# Define the bucket
 bucket = s3.Bucket(bucket_name)
 print(bucket)
 
+# Subscribe to the kafka topic
 consumer.subscribe([kafka_topic])
+
+# Loop over messages in the kafka topic and put files to S3 bucket
 for msg in consumer:
     data = msg.value
     save_file_name = data.get("unique_id") + ".json"
     save_path = os.path.join(folder_name, save_file_name)
+    print(f"Saving file to {save_path}")
     s3object = bucket.put_object(Key=save_path, Body=dumps(data))
