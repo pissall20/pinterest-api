51febd09b690f057b8e96eca1865e5285828a76e
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-16 21:01:10 +0530

    Saving files locally to json_files/ folder

--- helpers/boto_connection.py
+++ helpers/boto_connection.py
@@ -13,7 +13,7 @@ s3 = boto3.resource(
     aws_secret_access_key=aws_secret_key
 )
 
-bucket = s3.Bucket('pintrst')
+bucket = s3.Bucket(bucket_name)
 print(bucket)
 
 # s3.Object('pintrst', 'pinpost/image_traits.csv').put(Body=open('/Users/pissall/git/general-coding/Pinterest_App/helpers/image_traits.csv', 'rb'))
@@ -25,6 +25,12 @@ list_of_files = list()
 
 for buck_object in bucket.objects.all():
     print(buck_object.key)
+
+    if buck_object.key == f"{folder_name}/":
+        continue
     list_of_files.append(buck_object.key)
+    save_local_file = buck_object.key.split("/")[1]
+    local_file_path = os.path.join(os.getcwd(), "json_files", save_local_file)
+    bucket.download_file(buck_object.key, local_file_path)
 
 print(len(list_of_files))
