f93fc31e431c1de234dfb51199e2f74a971be0e0
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-17 02:09:31 +0530

    Streaming consumer ready till parsing data

--- Consumers/streaming_consumer.py
+++ Consumers/streaming_consumer.py
@@ -1,13 +1,39 @@
-from kafka import KafkaConsumer
-from json import loads
+import pyspark
+import pyspark.sql.functions as F
+from pyspark.sql import SparkSession
+from pyspark.sql.types import *
 
+packages = ['org.apache.spark:spark-sql-kafka-0-10_2.12:{}'.format(pyspark.__version__)]
+packages = ",".join(packages) if isinstance(packages, list) else packages
+spark = (
+    SparkSession.builder.master("local[*]")
+        .config("spark.io.compression.codec", "snappy")
+        .config("spark.ui.enabled", "false")
+        .config("spark.jars.packages", packages)
+        .getOrCreate()
+)
 
-consumer = KafkaConsumer(
-    bootstrap_servers=['localhost:9092'],
-    auto_offset_reset='earliest',
-    consumer_timeout_ms=10000
-    )
+kafka_df = spark.readStream \
+    .format("kafka") \
+    .option("kafka.bootstrap.servers", "localhost:9092") \
+    .option("failOnDataLoss", "false") \
+    .option("subscribe", "pinterest") \
+    .option("includeHeaders", "true") \
+    .option("startingOffsets", "latest") \
+    .option("spark.streaming.kafka.maxRatePerPartition", "50") \
+    .load()
 
-consumer.subscribe(['test-topic'])
-for msg in consumer:
-    print(msg)
+kafka_df.printSchema()
+
+string_df = kafka_df.selectExpr("CAST(value AS STRING)")
+
+schema = StructType().add("category", "string").add("index", "integer").add("unique_id", "string") \
+    .add("title", "string").add("description", "string").add("follower_count", "string").add("tag_list", "string") \
+    .add("is_image_or_video", "string").add("image_src", "string").add("downloaded", "integer") \
+    .add("save_location", "string")
+
+schema_df = string_df.select(F.from_json(F.col("value"), schema).alias("data")).select("data.*")
+
+df1 = schema_df.withColumn("save_location", F.expr("substring(save_location, 16, length(save_location))"))
+
+df1.writeStream.format("console").outputMode("append").start().awaitTermination()
