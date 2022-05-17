3a956ead9ab3cda38010ee88ff4b9ce5da7bfa57
Siddhesh Pisal <pisalsiddhesh@gmail.com>
2022-05-17 17:44:51 +0530

    Added transformations

--- Consumers/streaming_consumer.py
+++ Consumers/streaming_consumer.py
@@ -34,6 +34,20 @@ schema = StructType().add("category", "string").add("index", "integer").add("uni
 
 schema_df = string_df.select(F.from_json(F.col("value"), schema).alias("data")).select("data.*")
 
+# Clean save_location column to only contain the path
 df1 = schema_df.withColumn("save_location", F.expr("substring(save_location, 16, length(save_location))"))
 
-df1.writeStream.format("console").outputMode("append").start().awaitTermination()
+# Clean follower count and convert to integer (k=1000, M=100000)
+df2 = df1.withColumn("follower_count", F.regexp_replace(F.col("follower_count"), "k", "000"))\
+    .withColumn("follower_count", F.regexp_replace(F.col("follower_count"), "M", "000000"))\
+    .withColumn("follower_count", F.col("follower_count").cast("integer"))
+
+# Convert tag_list which is as string to an actual list of tags
+df3 = df2.withColumn("tag_list_clean", F.split(F.col("tag_list"), ","))
+
+# Count the number of tags as a feature
+df4 = df3.withColumn("number_of_tags", F.size(F.col("tag_list_clean")))
+
+df4.printSchema()
+
+df4.writeStream.format("console").outputMode("append").start().awaitTermination()
